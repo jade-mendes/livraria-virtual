62e7f8015da25e9beaf0759126a70cab696e8b52
Jade <jdmsa3@gmail.com>
2025-03-05 12:13:49 -0300

    feat(Login): validação dos campos do formulário

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,33 @@
 import imgPath  from '/assets/login-banner.jpeg'
 import logo from '/assets/logo.svg'
 import styles from './styles.module.css'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useNavigate } from "react-router";
 
 export default function Login(){
+    const navigate = useNavigate();
+    const loginSchema = z.object({
+        email: z.string().nonempty('O e-mail não pode ser vazio').refine
+        (value => z.string().email().safeParse(value).success, {
+            message: 'E-mail inválido'
+        }),
+        password: z.string().nonempty('A senha não pode ser vazia').min(6, 'A senha deve ter, no mínimo, 6 caracteres'),
+
+    })
+
+    const { register, handleSubmit, reset, formState: {errors, isSubmitting} } = useForm({
+        resolver: zodResolver(loginSchema)
+    });
+
+    async function logUser(data: any){
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log(data);
+        reset();
+        navigate('/');
+    }
+
     return (
         <div className={styles.loginContainer}>
 
@@ -16,19 +41,30 @@ export default function Login(){
                     <h4>Bem vindo(a)!</h4>
                     <h2>Entre na sua conta</h2>
 
-                    <form className={styles.form}>
+                    <form onSubmit={handleSubmit(logUser)} className={styles.form}>
                         <div className={styles.inputGroup}>
                             <label>E-mail</label>
-                            <input type='email' placeholder='Digite aqui seu e-mail'/>
+                            <input 
+                                type='email' 
+                                placeholder='Digite aqui seu e-mail'
+                                {...register('email')}
+                            />
+                            {errors.email && <span>{errors.email.message}</span>}
                         </div>
 
                         <div className={styles.inputGroup}>
                             <label>Senha</label>
-                            <input type='password' placeholder='Digite aqui sua senha'/>
+                            <input 
+                                type='password' 
+                                placeholder='Digite aqui sua senha'
+                                {...register('password')}
+                            />
                         </div>
                         <div className={styles.buttonsGroup}>
-                            <button className={styles.loginButton}>Entrar</button>
-                            <button className={styles.registerButton}>Cadastre-se</button>
+                            <button disabled={isSubmitting} type="submit" className={styles.loginButton}>
+                                {isSubmitting ? 'Carregando...' : 'Entrar'}
+                            </button>    
+                            <button type="button" className={styles.registerButton}>Cadastre-se</button>
                         </div>
                     </form>
                 </div>
